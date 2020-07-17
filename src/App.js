fadbb04cafde7b15ff765d1b015fec000f3d47cb
hraya <hraya@lssu.edu>
2020-07-16 23:28:35 -0500

    added starter header and post info

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import Post from './components/Post';
-import './css/App.css';
+import React from "react";
+import Post from "./components/Post";
+import "./css/App.css";
 
 function App() {
   return (
-    <div className="App">
-    <h1>This is a header</h1>
-     <Post />
+    <div className="app">
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.pmg"
+          alt="Insta Logo"
+        />
+      </div>
+      <Post />
     </div>
   );
 }
