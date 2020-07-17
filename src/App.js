780d849fc57908de217dcf36ca28c6f28345d3e0
hraya <hraya@lssu.edu>
2020-07-17 01:11:53 -0500

    added dummy area data for tests

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Post from "./components/Post";
 import "./css/App.css";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: "bossman",
+      caption: "This is coming from props",
+      imageUrl:
+        "https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"
+    },
+    {
+      username: "bossman2",
+      caption: "This is coming from props2",
+      imageUrl:
+        "https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"
+    }
+  ]);
   return (
     <div className="app">
       <div className="app__header">
@@ -12,11 +26,13 @@ function App() {
           alt="Insta Logo"
         />
       </div>
-      <Post
-        username="bossman"
-        caption="This is coming from props"
-        imageUrl="https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"
-      />
+      {posts.map(post => (
+        <Post
+          username={post.username}
+          caption={post.caption}
+          imageUrl={post.imageUrl}
+        />
+      ))}
     </div>
   );
 }
