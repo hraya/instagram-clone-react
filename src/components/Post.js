67c61b6247ef1b12d1d1512d6ea50f97d85887aa
hraya <hraya@lssu.edu>
2020-07-16 22:50:09 -0500

    Initial commit
    with some starter code

---
+++ src/components/Post.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../css/Post.css";
+
+const Post = () => {
+  return (
+    <div>
+      <h3>Username</h3>
+      <img
+        className="post__image"
+        src="https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"
+        alt="img"
+      />
+
+      <h4>caption under image</h4>
+    </div>
+  );
+};
+
+export default Post;
